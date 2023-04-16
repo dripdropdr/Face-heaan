dbad00d6fb3f5e80ca73badee1c91bb67e655fd5
dripdropdr <workingsumin@gmail.com>
2023-04-16 16:10:26 +0900

    first commit, arcface data load for inference

--- test.py
+++ test.py
@@ -15,77 +15,56 @@ from config import Config
 from torch.nn import DataParallel
 
 
-def get_lfw_list(pair_list):
-    with open(pair_list, 'r') as fd:
-        pairs = fd.readlines()
-    data_list = []
-    for pair in pairs:
-        splits = pair.split()
+# def get_lfw_list(pair_list):
+#     with open(pair_list, 'r') as fd:
+#         pairs = fd.readlines()
+#     data_list = []
+#     for pair in pairs:
+#         splits = pair.split()
 
-        if splits[0] not in data_list:
-            data_list.append(splits[0])
+#         if splits[0] not in data_list:
+#             data_list.append(splits[0])
 
-        if splits[1] not in data_list:
-            data_list.append(splits[1])
-    return data_list
+#         if splits[1] not in data_list:
+#             data_list.append(splits[1])
+#     return data_list
 
+# def load_model(model, model_path):
+#     model_dict = model.state_dict()
+#     pretrained_dict = torch.load(model_path)
+#     pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
+#     model_dict.update(pretrained_dict)
+#     model.load_state_dict(model_dict)
 
 def load_image(img_path):
-    image = cv2.imread(img_path, 0)
-    if image is None:
-        return None
-    image = np.dstack((image, np.fliplr(image)))
-    image = image.transpose((2, 0, 1))
-    image = image[:, np.newaxis, :, :]
-    image = image.astype(np.float32, copy=False)
-    image -= 127.5
-    image /= 127.5
-    return image
+    img = cv2.imread(img_path, 0) # 컬러는 상관 없어서 ?
+    if img is None:
+        raise 'read {} error'.format(img_path)
+    img = cv2.resize(img, (250, 250), interpolation=cv2.INTER_CUBIC)
+    img = np.dstack((img, np.fliplr(img)))
+    img = img.transpose((2, 0, 1))
+    img = img[:, np.newaxis, :, :]
+    img = img.astype(np.float32, copy=False)
+    img -= 127.5
+    img /= 127.5
+    return img
 
 
-def get_featurs(model, test_list, batch_size=10):
-    images = None
-    features = None
-    cnt = 0
-    for i, img_path in enumerate(test_list):
-        image = load_image(img_path)
-        if image is None:
-            print('read {} error'.format(img_path))
+def get_features(model, img_path, opt, device):
 
-        if images is None:
-            images = image
-        else:
-            images = np.concatenate((images, image), axis=0)
+    img = load_image(img_path)
+    cnt += 1
 
-        if images.shape[0] % batch_size == 0 or i == len(test_list) - 1:
-            cnt += 1
+    data = torch.from_numpy(img)
+    data = data.to(device)
+    output = model(data)
+    # output = output.data.cpu().numpy()
 
-            data = torch.from_numpy(images)
-            data = data.to(torch.device("cuda"))
-            output = model(data)
-            output = output.data.cpu().numpy()
+    fe_1 = output[::2]
+    fe_2 = output[1::2]
+    feature = np.hstack((fe_1, fe_2))
 
-            fe_1 = output[::2]
-            fe_2 = output[1::2]
-            feature = np.hstack((fe_1, fe_2))
-            # print(feature.shape)
-
-            if features is None:
-                features = feature
-            else:
-                features = np.vstack((features, feature))
-
-            images = None
-
-    return features, cnt
-
-
-def load_model(model, model_path):
-    model_dict = model.state_dict()
-    pretrained_dict = torch.load(model_path)
-    pretrained_dict = {k: v for k, v in pretrained_dict.items() if k in model_dict}
-    model_dict.update(pretrained_dict)
-    model.load_state_dict(model_dict)
+    return feature
 
 
 def get_feature_dict(test_list, features):
@@ -136,16 +115,16 @@ def test_performance(fe_dict, pair_list):
     return acc, th
 
 
-def lfw_test(model, img_paths, identity_list, compair_list, batch_size):
+def lfw_test(model, img_paths, compair_list, batch_size):
     s = time.time()
-    features, cnt = get_featurs(model, img_paths, batch_size=batch_size)
+    features = get_features(model, img_paths, batch_size=batch_size)
     print(features.shape)
     t = time.time() - s
     print('total time is {}, average time is {}'.format(t, t / cnt))
     fe_dict = get_feature_dict(identity_list, features)
-    acc, th = test_performance(fe_dict, compair_list)
-    print('lfw face verification accuracy: ', acc, 'threshold: ', th)
-    return acc
+    # acc, th = test_performance(fe_dict, compair_list)
+    # print('lfw face verification accuracy: ', acc, 'threshold: ', th)
+    return fe_dict
 
 
 if __name__ == '__main__':
@@ -158,16 +137,17 @@ if __name__ == '__main__':
     elif opt.backbone == 'resnet50':
         model = resnet50()
 
+    cpu = torch.device("cpu")
     model = DataParallel(model)
-    # load_model(model, opt.test_model_path)
-    model.load_state_dict(torch.load(opt.test_model_path))
-    model.to(torch.device("cuda"))
-
-    identity_list = get_lfw_list(opt.lfw_test_list)
-    img_paths = [os.path.join(opt.lfw_root, each) for each in identity_list]
-
+    model.load_state_dict(torch.load(opt.test_model_path, map_location=torch.device('cpu')))
+    model.to(cpu)
     model.eval()
-    lfw_test(model, img_paths, identity_list, opt.lfw_test_list, opt.test_batch_size)
+
+    img_paths = [os.path.join(opt.lfw_root, each) for each in os.listdir('face_img')]
+
+    cnt = 0
+    for i, img_path in enumerate(img_paths):
+        lfw_test(model, img_path, opt, cpu)
 
 
 
